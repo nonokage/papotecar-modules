ee8417a1f030f6c4ffd319bdfb120d505ceb6937
nonokage <nononokage@gmail.com>
2018-04-23 22:16:14 +0200

    Initialisation du projet de test des différents modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { TestViewComponent } from './test-view/test-view.component';
+import { ConnectComponent } from './connect/connect.component';
+import { FooterComponent } from './footer/footer.component';
+
+// creation des routes / url
+const appRoutes: Routes = [
+  { path: 'test', component: TestViewComponent },
+  { path: 'connect', component: ConnectComponent },
+  { path: '', component: TestViewComponent }
+];
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    TestViewComponent,
+    ConnectComponent,
+    FooterComponent
   ],
   imports: [
-    BrowserModule
-  ],
+    BrowserModule,
+    RouterModule.forRoot(appRoutes,
+      { enableTracing: true } // <-- debugging purposes only
+    )],
   providers: [],
   bootstrap: [AppComponent]
 })
