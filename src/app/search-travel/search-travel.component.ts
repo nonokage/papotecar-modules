a47fb5693e2b0e398dc63d2397ea73c357138903
nonokage <nononokage@gmail.com>
2018-04-24 20:04:05 +0200

    Ajout de component

---
+++ src/app/search-travel/search-travel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-search-travel',
+  templateUrl: './search-travel.component.html',
+  styleUrls: ['./search-travel.component.scss']
+})
+export class SearchTravelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
